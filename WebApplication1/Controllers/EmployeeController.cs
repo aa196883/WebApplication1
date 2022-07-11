41237cb2227c488609f10e34fbfccdc9f0fd2dfa
aa196883 <aaly@enssat.fr>
2022-07-11 16:51:01 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableCors]
+    public class EmployeeController : Controller
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet(Name = "GetEmployee")]
+        public IEnumerable<Employee> Get()
+        {
+            _logger.LogInformation("information");
+            IEnumerable<Employee> data = Enumerable.Range(1, 10).Select(index => new Employee
+            {
+                Id = Random.Shared.Next(0, 100),
+                Name = "a"
+            })
+            .ToArray();
+            return data;
+        }
+
+        [HttpGet("{id}")]
+        public Employee GetById(int id)
+        {
+            return new Employee
+            {
+                Id = id,
+                Name = ((char) id).ToString()
+            };
+        }
+    }
+}
